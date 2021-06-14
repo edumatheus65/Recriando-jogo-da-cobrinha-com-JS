e112c6bf2757da4017804b70ffe8089b30137515
edumatheus65 <77867650+edumatheus65@users.noreply.github.com>
2021-06-14 09:09:49 -0300

    definir aleatoriedade da frutinha e aumento da cobrinha

--- script.js
+++ script.js
@@ -45,16 +45,26 @@ function iniciarJogo() //função que atualiza o jogo de tempos em tempos para q
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
     if(direction == "right") snakeX += box;
-    if(direction == "left") snakex -= box;
+    if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
+
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+
+    }
 
     snake.pop(); // retira o ultimo elemento do wayhave
 
